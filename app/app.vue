4a6c018759b10407de4e3c8b2c4490997221263f
steffkes <stefan@gelungen.es>
2026-09-23 14:41:54 +0200

    configure prettier

--- app/app.vue
+++ app/app.vue
@@ -8,33 +8,32 @@ useHead({
 </script>
 
 <template>
-<section class="section py-6">
-
+  <section class="section py-6">
     <div class="columns">
-        <div class="column is-10 is-offset-1">
+      <div class="column is-10 is-offset-1">
+        <h1 class="title">Gelungenes</h1>
+        <p class="subtitle">Wettkämpfe, die man nicht so schnell vergisst.</p>
 
-            <h1 class="title">Gelungenes</h1>
-            <p class="subtitle">Wettkämpfe, die man nicht so schnell vergisst.</p>
+        <div class="content">
+          <p>
+            Gelungenes steht für außergewöhnliche Sport- und
+            Wettkampfveranstaltungen. Für Herausforderungen, bei denen es auf
+            Leistung, Teamgeist und den Willen ankommt, bis zum Schluss
+            durchzuziehen.
+          </p>
 
-            <div class="content">
+          <p>
+            Was 2023 mit dem Schanzenlauf Oberstdorf begann, ist inzwischen eine
+            Reihe ganz unterschiedlicher Wettkämpfe – vom Treppenlauf in
+            spektakulärer Kulisse bis zum Berglauf durch die Allgäuer
+            Landschaft.
+          </p>
 
-                <p>Gelungenes steht für außergewöhnliche Sport- und Wettkampfveranstaltungen.
-                Für Herausforderungen, bei denen es auf Leistung, Teamgeist und den Willen
-                ankommt, bis zum Schluss durchzuziehen.</p>
-
-                <p>Was 2023 mit dem Schanzenlauf Oberstdorf begann, ist inzwischen eine Reihe
-                ganz unterschiedlicher Wettkämpfe – vom Treppenlauf in spektakulärer Kulisse
-                bis zum Berglauf durch die Allgäuer Landschaft.</p>
-
-                <p>Anspruchsvoll. Ungewöhnlich. Und ziemlich gelungen.</p>
-
-            </div>
-
+          <p>Anspruchsvoll. Ungewöhnlich. Und ziemlich gelungen.</p>
         </div>
+      </div>
     </div>
-
-</section>
-
+  </section>
 </template>
 
 <style>
